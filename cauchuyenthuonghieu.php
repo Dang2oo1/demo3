57bd3f8298e099e5431158df760147d927c5ddf1
Hai Dang <haidang@gmail.com>
2022-06-18 10:24:36 +0700

    dangclick

---
+++ cauchuyenthuonghieu.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Câu chuyện thương hiệu</title>
+    <script src="https://kit.fontawesome.com/13f561a656.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./Font-Awesomecss/css/font-awesome.min.css">
+    <link href="./cauchuyenthuonghieu.css" rel="stylesheet">
+</head>
+<body>
+<iframe  width="100%" height="315" frameborder="0";  
+src="https://www.youtube.com/embed/R7IXCJSjuX8?controls=0&mute=1&loop=1&autoplay=1" >
+</iframe>
+
+<div>
+    <div class="icon">
+      <a class="a-icon" href="./index.php">  <i class="fa-solid fa-house" style="color:#FB9D43 " ></i></a> <span class="span-icon">| Câu chuyện thương hiệu</span>
+    </div>
+</div>
+
+<div class="container">
+<div class="content-1">
+        <div class="detail-content-1">
+            <h3>Câu chuyện thương hiệu</h3>
+            <div class="detail-content-1-1">
+               <div>
+               <ul class="title-content1">
+                    <li>Đúng như cái tên “Feeling Tea” – Trà sữa của cảm xúc, của những rung động vị giác đầy hứng khởi và tràn đầy sức sống</li>
+                    <li>Với mong muốn mang đến hương vị trà sữa tuyệt hảo và trọn vẹn nhất – Feeling Tea đã ra đời. Vị ngọt thanh mà không ngán, vị đắng nhẹ của trà hòa quyện vị béo của sữa tạo nên hương vị quyến rũ, lưu giữ tận sâu trong từng tế bào.</li>
+                    <li>
+Với việc sáng tạo không ngừng và cho ra đời những dòng thức uống mới lạ, Feeling Tea  sẽ giúp các bạn được phiêu lưu trong những chuyến hành trình mới mẻ và thú vị về thế giới trà.</li>
+                </ul>
+               </div>
+               
+               <div>
+                   <img src="./img/thuonghieu1.png" alt="" width="383px" height="383px" style="padding-right: 100px;">
+               </div>
+            </div>
+
+        </div>
+</div>
+
+            <div class="content-2">
+                    <h3 class="detail-content-2">
+                    Love your feeling</h3>               
+            </div>
+
+            <div class="content-3">
+                <div class="detail-content-3">
+            
+            
+            <div class="detail-content-3-1">
+            <div>
+                   <img src="./img/thuonghieu2.jpg" alt="" width="383px" height="373px" style="padding-right:50px;">
+               </div>
+               <div>
+               <h3 class="h3-content3">feeling tea</h3>
+               <ul class="title-content3">
+                    <li>Ai bảo Feeling Tea chỉ là một ly trà sữa? Một ly trà của Feeling Tea còn là phương thuốc giúp chữ lành vết thương, là thần dược xoa dịu tâm hồn, là những nốt nhạc trầm đẹp đẽ của cuộc sống này.</li>
+                    <li>
+                    Hãy mở lòng và giành thời gian ở bên nhau, để thấy phút giây dành cho nhau còn quý hơn cả những lời yêu thương ngọt ngào.</li>
+                    <li>Ngồi bên nhau nhâm nhi ly trà sữa Feeling Tea để cảm nhận thế nào là bình yên bạn nhé</li>
+                </ul>
+               </div>
+               
+        
+                 </div>
+
+                </div>
+            </div>
+
+            <div class="content-4">
+                <div class="detail-content-4">
+                    <div class="detail-content-4-1">
+                        <img src="./img/thuonghieu3.jpg" width="500px" height="780px" alt="">
+                    </div>
+                    <div class="detail-content-4-2">
+                        <div><img src="./img/thuonghieu4.jpg" width="550px" height="367px" alt=""></div>
+                        <div style="margin-top:20px"><img src="./img/thuonghieu5.jpg" width="550px" height="367px" alt=""></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="content-5">
+                <div class="detail-content-5">
+                    <div>
+                        <h3 class="detail-content-5-1">  Giá trị cốt lõi</h3> 
+                    </div>
+
+                    <div class="detail-content-5-2">
+                        <div class="detail-content-5-3">
+                            <div class="icon-content5"><i class="fa-solid fa-medal"></i></div>
+                            <div class="word-content5">
+                                <h3>Chất lượng hàng đầu</h3>
+                                <p>An toàn, vệ sinh và ngon miệng. Việc sử dụng các</br> nguyên liệu an toàn, tự nhiên và vệ sinh là ưu tiên</br> hàng đầu của Feeling Tea. Hương vị tuyệt hảo của</br> các món thức uống là mục đích quan trọng tiếp </br>theo mà chúng tôi muốn hướng đến.</p>
+                            </div>
+                        </div>
+                        <div class="detail-content-5-3">
+                            <div class="icon-content5"><i class="fa-solid fa-heart"></i></div>
+                            <div  class="word-content5">
+                                <h3>Dịch vụ thân thiện & chuyên nghiệp</h3>
+                                <p>Feeling Tea mong muốn làm hài lòng khách hàng</br> tốt nhất với tác phong phục vụ chuyên nghiệp và </br>thân thiện, luôn lắng nghe những đóng góp của</br> khách hàng.</p>
+                            </div>
+                        </div>
+                        <div class="detail-content-5-3">
+                            <div class="icon-content5"><i class="fa-solid fa-thumbs-up"></i></div>
+                            <div  class="word-content5">
+                                <h3>Thương hiệu đáng tin cậy</h3>
+                                <p>Dựa trên hai nền tảng cốt lõi là Chất Lượng và</br> Dịch Vụ, Feeling Tea luôn nỗ lực xây dựng và duy</br> trì hình ảnh thương hiệu đáng tin cậy trong mắt </br>khách hàng.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>            
+            </div>
+
+            <div class="content-6">
+                <div class="detail-content-6">
+                    <div><img  style="padding:10px 0px 0px 110px;" src="./img/thuonghieu6.jpg" alt=""  width="550px" height="825px"></div>
+                    <div><img  style="padding:10px 120px 0px 0px" src="./img/thuonghieu7.jpg" alt=""  width="550px" height="825px"></div>
+                </div>
+            </div>
+
+            <div class="content-7">
+                <div class="detail-content-7">
+                    <div class="detail-content-7-1">
+                        <h2 >Quá trình phát triển</h2>
+                        <p>Qua quá trình hoạt động công ty TNHH Cảm xúc  – đơn vị nhượng quyền độc quyền của Feeling Tea tại Việt Nam, đã đưa</br> thương hiệu phát triển nhanh chóng trở thành một trong những điểm đến thân thuộc của các bạn trẻ yêu thích văn hóa trà sữa.</p>     
+                    </div>
+                    <div><span class="sp">_____________________________________________________________________________________________________________________________________________</span></div>
+                </div>
+            </div>
+</div>
+</body>
+
+<footer>
+    <div class="container-footer">
+        <div class="content-footer">
+            <div>
+                <a href="./index.php">
+                    <img src="./img/footer.png" alt="" width="265px" height="265px" >   
+                </a>
+            </div>
+            <div class="Menu-footer">
+                <div class="title-footer"><h2>Menu</h2></div>
+                <div>
+                    <ul class="menu">
+                        <li>
+                            <a href="./index.php">Trang Chủ</a>
+                        </li>
+                        <li> 
+                            <a href="./cauchuyenthuonghieu.php">Câu chuyện thương hiệu</a>
+                        </li>
+                        <li>
+                            <a href="./thucdon.php">Thực Đơn</a>
+                        </li>
+                        <li>
+                            <a href="./tintuc.php">Tin Tức</a>
+                        </li>
+                        <li>
+                            <a href="./cuahang.php">Hệ thống cửa hàng</a>
+                        </li>
+                        <li>
+                            <a href="./tuyendung.php">Tuyển Dụng</a>
+                        </li>
+                        <li>
+                            <a href="./nhuongquyen.php">Nhượng Quyền</a>
+                        </li>
+                        <li>
+                            <a href="./uudaithanhvien.php">Ưu đãi thành viên</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="lienhe-footer">
+                <div class="title-footer"><h2>Liên Hệ</h2></div>
+                <div class="word-footer">
+                    <p>Trụ sở chính: 155 Phố Giảng Võ, quận </br> Đống Đa, TP Hà Nộ</p>
+                    <p>Hotline: 0982.294.133</p>
+                    <p>Email: feelingtea.marketing@gmail.com</p>
+                </div>
+            </div>
+            <div class="ketnoi-footer">
+                <div class="title-footer"><h2>Kết nối</h2></div>
+                <div id="link">
+                    <a href="https://www.facebook.com/" class="fa fa-facebook"></a>                   
+                    <a href="https://twitter.com/" class="fa fa-twitter"></a>
+                    <a href="https://www.youtube.com/" class="fa fa-youtube"></a>
+                    <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+</html>
