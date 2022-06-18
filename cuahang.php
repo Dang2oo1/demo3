dfdbcac96bdf967066eadf5390e772292fe617b8
Hai Dang <haidang@gmail.com>
2022-06-18 10:26:58 +0700

    longclick

---
+++ cuahang.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cửa hàng</title>
+    <link href="./cuahang.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/13f561a656.js"></script>
+    <link rel="stylesheet" href="./Font-Awesomecss/css/font-awesome.min.css">
+</head>
+
+<header>
+        <div class="first-head">
+            <h2>Hệ thống cửa hàng Feelingtea</h2>
+        </div>
+</header>
+
+<body>
+    <div class="container">
+        <div class="container-1">
+            <div>
+                <div class="icon">
+                    <a class="a-icon" href="./index.php">  <i class="fa-solid fa-house" style="color:#FB9D43 " ></i></a> <span class="span-icon">| Hệ thống cửa hàng</span>
+                </div>
+            </div>
+
+            <div class="search">
+            <select id="HaNoi">
+                <option value="timkiem">Tìm cửa hàng theo quận</option>
+                <option value="badinh">Ba Đình</option>
+                <option value="bactuliem">Bắc Từ Liêm</option>
+                <option value="caugiay">Cầu Giấy</option>
+                <option value="dongda">Đống Đa</option>
+                <option value="hadong">Hà Đông</option>
+                <option value="haibatrung">Hai Bà Trưng</option>
+                <option value="hoankiem">Hoàn Kiếm</option>
+                <option value="hoangmai">Hoàng Mai</option>
+                <option value="longbien">Long Biên</option>
+                <option value="namtuliem">Nam Từ Liêm</option>
+                <option value="tayho">Tây Hồ</option>
+                <option value="thanhxuan">Thanh Xuân</option>
+            </select>
+            </div>
+
+            <div class="store">
+                <div class="content-store">
+                    <div class="img-store"><img src="./img/nguyenchithanh.jpg" alt="" width="330px" height="196px"></div>
+                    <div class="under-content-store">
+                        <h2>Feeling Tea Nguyễn Chí Thanh</h2>
+                    </div>
+                    <div class="under-content2-store">
+                        <p>122 Nguyễn Chí Thanh</p>
+                        <p>0896686196</p>
+                        <p>08:00 – 22:00</p>
+                    </div>
+                    <div class="btn-store">
+                        <form action="https://goo.gl/maps/wHRSRrmAq3zWMdUx9" target="_blank">
+                            <button class="btn">Tìm Đường</button>
+                        </form>
+                    </div>
+                </div> 
+                <div class="content-store">
+                    <div class="img-store"><img src="./img/phamngocthach.jpg" alt="" width="313px" height="196px"></div>
+                    <div class="under-content-store">
+                        <h2>Feelingtea Phạm Ngọc Thạch</h2>
+                    </div>
+                    <div class="under-content2-store">
+                        <p>100 Phạm Ngọc Thạch, Đống Đa, Hà Nội</p>
+                        <p>0243 2008028</p>
+                        <p>08:00 – 22:00</p>
+                    </div>
+                    <div class="btn-store">
+                        <form action="https://goo.gl/maps/Ks3hgCHpdmUrxniJ7" target="_blank">
+                            <button class="btn">Tìm Đường</button>
+                        </form>
+                    </div>
+                </div>      
+                <div class="content-store">
+                    <div class="img-store"><img src="./img/luongthevinh.jpg" alt="" width="313px" height="196px"></div>
+                    <div class="under-content-store">
+                        <h2>Feelingtea Lương Thế Vinh</h2>
+                    </div>
+                    <div class="under-content2-store">
+                        <p>48 Lương Thế Vinh, Thanh Xuân, Hà Nội</p>
+                        <p>0243 2009506</p>
+                        <p>08:00 – 22:00</p>
+                    </div>
+                    <div class="btn-store">
+                        <form action="https://goo.gl/maps/EfS9DKgx9zSj1aDU6" target="_blank">
+                            <button class="btn">Tìm Đường</button>
+                        </form>     
+                    </div>
+                </div>                     
+            </div>
+
+            <div class="store">
+                <div class="content-store">
+                    <div class="img-store"><img src="./img/nguyenvancu.jpg" alt="" width="330px" height="196px"></div>
+                    <div class="under-content-store">
+                        <h2>Feelingtea Nguyễn Văn Cừ</h2>
+                    </div>
+                    <div class="under-content2-store">
+                        <p>192 Nguyễn Văn Cừ, Long Biên, Hà Nội</p>
+                        <p>0243 2161552</p>
+                        <p>08:00 – 22:00</p>
+                    </div>
+                    <div class="btn-store">
+                        <form action="https://goo.gl/maps/aTHLspov9f5D6YQg6" target="_blank">
+                            <button class="btn">Tìm Đường</button>
+                        </form>
+                    </div>
+                </div> 
+                <div class="content-store">
+                    <div class="img-store"><img src="./img/levanhien.jpg" alt="" width="313px" height="196px"></div>
+                    <div class="under-content-store">
+                        <h2 style="padding-left: 40px;">Feelingtea Lê Văn Hiến</h2>
+                    </div>
+                    <div class="under-content2-store">
+                        <p>23 Lê Văn Hiến, Bắc Từ Liêm, Hà Nội</p>
+                        <p>0243 7553447</p>
+                        <p>08:00 – 22:00</p>
+                    </div>
+                    <div class="btn-store">
+                        <form action="https://goo.gl/maps/nb3Rsh12Lb5dYN1i9" target="_blank">
+                            <button class="btn">Tìm Đường</button>
+                        </form>
+                    </div>
+                </div>      
+                <div class="content-store">
+                    <div class="img-store"><img src="./img/tanmai.jpg" alt="" width="313px" height="196px"></div>
+                    <div class="under-content-store">
+                        <h2 style="padding-left: 60px;">Feelingtea Tân Mai</h2>
+                    </div>
+                    <div class="under-content2-store">
+                        <p>128 Tân Mai, Hoàng Mai, Hà Nội</p>
+                        <p>0243 2181098</p>
+                        <p>08:00 – 22:00</p>
+                    </div>
+                    <div class="btn-store">
+                        <form action="https://goo.gl/maps/U7FwPtypJRpTDDoh8" target="_blank">
+                            <button class="btn">Tìm Đường</button>
+                        </form>
+                    </div>
+                </div>                     
+            </div>
+           
+            <div class="container-2">
+                <div class="next-page">
+                    <a href="">1</a>
+                    <a href="">2</a>
+                    <a href="">3</a>
+                    <a href="">4</a>
+                    <a href="">5</a>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+</body>
+
+<footer>
+    <div class="container-footer">
+        <div class="content-footer">
+            <div>
+                <a href="./index.php">
+                    <img src="./img/footer.png" alt="" width="265px" height="265px" >   
+                </a>
+            </div>
+            <div class="Menu-footer">
+                <div class="title-footer"><h2>Menu</h2></div>
+                <div>
+                    <ul class="menu">
+                        <li>
+                            <a href="./index.php">Trang Chủ</a>
+                        </li>
+                        <li> 
+                            <a href="./cauchuyenthuonghieu.php">Câu chuyện thương hiệu</a>
+                        </li>
+                        <li>
+                            <a href="./thucdon.php">Thực Đơn</a>
+                        </li>
+                        <li>
+                            <a href="./tintuc.php">Tin Tức</a>
+                        </li>
+                        <li>
+                            <a href="./cuahang.php">Hệ thống cửa hàng</a>
+                        </li>
+                        <li>
+                            <a href="./tuyendung.php">Tuyển Dụng</a>
+                        </li>
+                        <li>
+                            <a href="./nhuongquyen.php">Nhượng Quyền</a>
+                        </li>
+                        <li>
+                            <a href="./uudaithanhvien.php">Ưu đãi thành viên</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="lienhe-footer">
+                <div class="title-footer"><h2>Liên Hệ</h2></div>
+                <div class="word-footer">
+                    <p>Trụ sở chính: 155 Phố Giảng Võ, quận </br> Đống Đa, TP Hà Nộ</p>
+                    <p>Hotline: 0982.294.133</p>
+                    <p>Email: feelingtea.marketing@gmail.com</p>
+                </div>
+            </div>
+            <div class="ketnoi-footer">
+                <div class="title-footer"><h2>Kết nối</h2></div>
+                <div id="link">
+                    <a href="https://www.facebook.com/" class="fa fa-facebook"></a>                   
+                    <a href="https://twitter.com/" class="fa fa-twitter"></a>
+                    <a href="https://www.youtube.com/" class="fa fa-youtube"></a>
+                    <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
